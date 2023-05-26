2c799212764d7b34b1e2d9cbc12be6153fe37e8c
Henrique Hallmann <henriquev@atlashxm.com>
2023-05-26 11:28:47 -0300

    good starting version with mui and storybook

---
+++ src/stories/MuiButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button as MuiButton } from '@mui/material';
+ 
+export interface ButtonProps {
+  label: string;
+}
+ 
+export const Button = ({ label, ...rest }: ButtonProps) => (
+  <MuiButton {...rest}>{label}</MuiButton>
+);
